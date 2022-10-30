db054b4eac9459a7c68cc19920104d4403f9763b
ajthms <ajthomas@AJs-MacBook-Pro.lan>
2022-10-30 15:15:56 -0400

    first

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const today = new Date()
+const userDate = document.getElementById("userDate")
+const userName = document.getElementById("userName")
+
+function formSubmit(event) {
+    event.preventDefault()
+    const [year,month,day] = userDate.value.split("-")
+    const dateValue = new Date(`${month}/${day}/${year}`)
+    const diff = Math.floor((today - dateValue) / (1000*60*60*24))
+    document.getElementById("results").innerHTML = `Great job, ${userName.value}`
+    document.getElementById("date").innerHTML = `You've been sober for ${diff} days!`
+    console.log(diff)
+
+}
+
+//display div to user, user cant select future date, save to local state
