6bf6df8e186e4c8c03de0a8a84a23d3b0fd2021d
ajthms <ajthomas@AJs-MacBook-Pro.local>
2023-01-03 18:16:30 -0500

    day

--- scripts.js
+++ scripts.js
@@ -1,23 +1,21 @@
 const today = new Date()
 const userDate = document.getElementById("userDate")
 const userName = document.getElementById("userName")
-localStorage.setItem('date', 'value');
-const value = localStorage.getItem('date');
+userName.value = localStorage.getItem('userName');
+userDate.value = localStorage.getItem('userDate');
 
 function formSubmit(event) {
     event.preventDefault()
     const [year,month,day] = userDate.value.split("-")
     const dateValue = new Date(`${month}/${day}/${year}`)
     const diff = Math.floor((today - dateValue) / (1000*60*60*24))
+    const dayLabel = diff===1 ? "day" : "days"
     document.getElementById("results").innerHTML = `Great job, ${userName.value}`
-    document.getElementById("date").innerHTML = `You've been sober for ${diff} days!`
-
+    document.getElementById("date").innerHTML = `You've been sober for ${diff} ${dayLabel}!`
+    localStorage.setItem('userName', userName.value);
+    localStorage.setItem('userDate', userDate.value);
 }
 
-function myFunction() {
-    const definition = document.getElementById("myDIV");
-    definition.classList.toggle("hidden")
-  }
 
   
-//display div to user, user cant select future date, save to local state
+//user cant select future date, click off calendar on click
